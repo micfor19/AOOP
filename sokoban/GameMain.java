f33fa28b273a040914b36c0b1db704e5f8462051
micfor19 <73466607+micfor19@users.noreply.github.com>
2021-06-13 14:50:16 +0200

    Add variables to GameMain.java
    Add general and game related variables

--- sokoban/GameMain.java
+++ sokoban/GameMain.java
@@ -4,6 +4,10 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import java.util.ArrayList;
+import java.io.*;
+import javax.sound.sampled.*;
+
 public class GameMain extends JPanel {	/**
 	 * 
 	 */
@@ -12,19 +16,141 @@ public class GameMain extends JPanel {	/**
 	
 	   // Define constants for the game
 	   static final String TITLE = "Sokoban";        // title of the game
-	   static final int CANVAS_WIDTH = 800;    // width and height of the drawing canvas
-	   static final int CANVAS_HEIGHT = 600;
 	   static final int UPDATES_PER_SEC = 4;    // number of game update per second
 	   static final long UPDATE_PERIOD_NSEC = 1000000000L / UPDATES_PER_SEC;  // nanoseconds
+	   static final int OFFSET = 0;
+	   static final int SPACE = 32;
+	
+	   // Define basic game variables
+	   private int w = 0;
+	   private int h = 0;
+
+	   private boolean legalPause = false;
+	   private boolean defaultSound = true;
 	   
 	// Enumeration for the states of the game.
 	   static enum GameState {
 	      INITIALIZED, PLAYING, PAUSED, GAMEOVER, DESTROYED
 	   }
+	// Enumeration for directions in a 2D environment
+	   static enum CollisionCheck	{
+		   UP, DOWN, LEFT, RIGHT
+	   }
+	   static CollisionCheck direction;
+	
+	// Enumeration for sound effects
+	   static enum SoundEffect {
+		   // customizable list of sounds for each game
+		   PUSH("d:/workspace/GameProject/freeze.wav"),
+		   STEP("d:/workspace/GameProject/koopa-stomp.wav"),
+		   SUCCESS("d:/workspace/GameProject/itemget.wav"),
+		   FINISH("d:/workspace/GameProject/itemreel.wav"),
+		   WALL("d:/workspace/GameProject/hit.wav");
+		   		   
+		// Nested class for specifying volume
+		   public static enum Volume {
+		      MUTE, LOW, MEDIUM, HIGH
+		   }
+		   
+		   public static Volume volume = Volume.LOW;
+		   
+		   // Each sound effect has its own clip, loaded with its own sound file.
+		   private Clip clip;
+		   
+		   // Constructor to construct each element of the enum with its own sound file.
+		   SoundEffect(String soundFileName) {
+		      try {
+		         // Use URL (instead of File) to read from disk and JAR.
+		         //URL url = this.getClass().getClassLoader().getResource(soundFileName);
+		    	  File f = new File(soundFileName);
+		         // Set up an audio input stream piped from the sound file.
+		         AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(f.toURI().toURL());
+		         // Get a clip resource.
+		         clip = AudioSystem.getClip();
+		         // Open audio clip and load samples from the audio input stream.
+		         clip.open(audioInputStream);
+		      } catch (UnsupportedAudioFileException e) {
+		         e.printStackTrace();
+		      } catch (IOException e) {
+		         e.printStackTrace();
+		      } catch (LineUnavailableException e) {
+		         e.printStackTrace();
+		      }
+		   }
+		   
+		   // Play or Re-play the sound effect from the beginning, by rewinding.
+		   public void play() {
+		      if (volume != Volume.MUTE) {
+		         if (clip.isRunning())
+		            clip.stop();   // Stop the player if it is still running
+		         clip.setFramePosition(0); // rewind to the beginning
+		         clip.start();     // Start playing
+		      }
+		   }
+		   
+		   // Optional static method to pre-load all the sound files.
+		   static void init() {
+		      values(); // calls the constructor for all the elements
+		   }
+	   }
 	   
 	   static GameState state;   // current state of the game
 	   
 	   private GameCanvas canvas;
+	
+	   // Declare menubar
+	   static JMenuBar menuBar;
+	
+	   // ------ All the game related variables here ------
+	
+	   // Game elements
+	   private Worker worker;
+	   private ArrayList<Wall> walls;
+	   private ArrayList<Crate> crates;
+	   private ArrayList<Target> targets;
+	   private ArrayList<Floor> spaces;
+	
+	   // Game Levels
+	   private String level1
+	   //* Level 1
+	   = "                    \n"
+	   + "                ### \n"
+	   + "                  # \n"
+	   + "     #######      # \n"
+	   + "     #     #      # \n"
+	   + "     #* $ @#      # \n"
+	   + "     #     #        \n"
+	   + "     #######        \n"
+	   + "                    \n"
+	   + "                    \n"
+	   + "                    \n";
+	   private String level2 
+	   = "     ########       \n"
+	   + "    ##@  #  ##  ####\n"
+	   + "    #    $   #     #\n"
+	   + "    ##  ###$ #    # \n"
+	   + "    #    #   #  ##  \n"
+	   + "    #    #   #  ####\n"
+	   + "    ###$   ###      \n"
+	   + "    ##     **#      \n"
+	   + "    #      *##      \n"
+	   + "    #########       \n"
+	   + "                    \n";
+	   private String level3
+	   //*	Level 2
+           = "    ######      ####\n"
+           + "    ##   #         #\n"
+           + "    ##$  #        ##\n"
+           + "  ####  $##        #\n"
+           + "  ##  $ $ #     ####\n"
+	   + "#### # ## #   ######\n"
+	   + "##   # ## #####  **#\n"
+	   + "## $  $          **#\n"
+	   + "###### ### #@##  **#\n"
+	   + "    ##     #########\n"
+	   + "    ########        \n";
+	
+	   private String level = level1;	// default level
 	   
 	   public GameMain()	{
 		   // Initialize the game objects
@@ -32,8 +158,8 @@ public class GameMain extends JPanel {	/**
 	   
 	      // UI components
 	      canvas = new GameCanvas();
-	      canvas.setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
-	      add(canvas);   // center of default BorderLayout
+	      canvas.setPreferredSize(new Dimension(w, h));
+	      add(canvas, BorderLayout.CENTER);   // center of default BorderLayout
 		      
 	   // Start the game.
 	      gameStart();  
